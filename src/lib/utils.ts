95b51ec7221d335f81345d5af82d671ae5b852be
XooTB <xo.ot2001@gmail.com>
2026-07-15 00:14:56 +0600

    feat: porting over curtains to astro from react

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,12 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function deviceType(width: number | undefined) {
-  if (width === undefined) return undefined;
-  if (width < 768) {
-    return "mobile";
-  } else if (width > 768) {
-    return "desktop";
-  }
-}
